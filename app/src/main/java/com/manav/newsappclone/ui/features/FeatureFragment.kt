f2f5f4a9a40148f667580e792a08161c5debfa0e
manav8913 <manav@renalyx.com>
2021-04-05 14:06:31 +0530

    Initial Setup

---
+++ app/src/main/java/com/manav/newsappclone/ui/features/FeatureFragment.kt
@@ -0,0 +1,26 @@
+package com.manav.newsappclone.ui.features
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.manav.newsappclone.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FeatureFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_feature, container, false)
+    }
+
+
+}
